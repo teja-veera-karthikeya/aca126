188a95e060f10565ee29624a229415175d423ea9
teja-veera-karthikeya <105000597+teja-veera-karthikeya@users.noreply.github.com>
2022-10-23 13:37:22 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+leftWristY=0;
+leftWristX=0;
+rightWristY=0;
+rightWristX=0;
 function preload(){
     song1 = loadSound("music.mp3");
     song2 = loadSound("music2.mp3");
@@ -9,6 +13,23 @@ function setup(){
 
     video=createCapture(VIDEO);
     video.hide();
+
+    poseNet=ml5.poseNet(video,modelloaded)
+    poseNet.on('pose',gotposes)
+}
+function modelloaded(){
+    console.log("PoseNet is intailized")
+}
+function gotposes(results){
+    if(results.length>0){
+        console.log(results)
+        leftWristY=results[0].pose.leftWrist.y;
+        leftWristX=results[0].pose.leftWrist.x;
+        console.log("leftWristY = "+leftWristY+"leftWristX = "+leftWristX)
+        rightWristY=results[0].pose.rightWrist.y;
+        rightWristX=results[0].pose.rightWrist.x;
+        console.log("rightWristY = "+rightWristY+"rightWristX = "+rightWristX)
+    }
 }
 function draw(){
     image(video,0,0,600,500);
